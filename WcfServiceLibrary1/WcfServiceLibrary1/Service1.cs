07f5a70164686899d8ae2d7565db06f4aa4f4969
WANGWei118 <wei.wang@etu.unice.fr>
2019-03-27 09:46:55 +0100

    add service, but not finished all the fonctions

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace WcfServiceLibrary1
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
+    public class Service1 : IService1
+    {
+        [WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json, UriTemplate = "data/{value}")]
+        public string GetData(string  value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
