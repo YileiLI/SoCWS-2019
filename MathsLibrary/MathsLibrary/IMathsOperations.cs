64e898c65efd8f038389e15e86b561eb64c09a27
WANGWei118 <wei.wang@etu.unice.fr>
2019-03-27 11:48:23 +0100

    service soap

---
+++ MathsLibrary/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        int Add(int num1, int num2);
+        [OperationContract]
+        int Multiply(int num1, int num2);
+    }
+}
